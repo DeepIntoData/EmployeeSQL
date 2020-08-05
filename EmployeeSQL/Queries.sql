7b525b39ef288a58f03bae733ea5bc50ad120396
frozte <deeppatel@live.com>
2020-08-05 01:36:11 -0400

    Update Queries.sql

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -1,12 +1,19 @@
 -- Queries
 
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
-
-
+SELECT emp_no, last_name, first_name, sex FROM employees --salaries --and groupby emp title id in employees to emp number in salaries
+WHERE emp_no in (
+	SELECT emp_no
+	FROM salaries 
+)
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
-
-
+SELECT s.salary FROM salaries s
+INNER JOIN employees e ON e.emp_no = s.emp_no
+WHERE emp_no in (
+	SELECT emp_no
+	FROM employees
+)
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
